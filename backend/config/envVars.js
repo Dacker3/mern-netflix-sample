47abb10d9911336ab713d4b64ef11d792d4b51c1
Dosreis Acker Jr <dr.dosacker@yahoo.com>
2025-02-18 11:39:33 -0600

    Added middleware, tv/movie routes, api fetch functions

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -7,4 +7,5 @@ export const ENV_VARS = {
     PORT: process.env.port || 5000,
     JWT_SECRET: process.env.JWT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
+    TPMD_API_KEY: process.env.TPMD_API_KEY,
 };
