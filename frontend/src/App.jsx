5b84bdd8a49f18f4ec7585b2a334e78cfa991dad
Dosreis Acker Jr <dr.dosacker@yahoo.com>
2025-02-27 22:48:22 -0600

    added search and search history page/ setup app production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import HomePage from "./pages/home/HomePage.jsx"
 import LoginPage from "./pages/LoginPage.jsx"
 import SignupPage from "./pages/SignupPage.jsx"
 import WatchPage from "./pages/WatchPage";
+import SearchPage from "./pages/SearchPage";
+import HistoryPage from "./pages/HistoryPage";
+import NotFoundPage from "./pages/404";
+
+
 
 import Footer from "./components/Footer.jsx"
 import { Toaster } from "react-hot-toast"
@@ -35,6 +40,11 @@ function App() {
         <Route path='/login' element={!user ? <LoginPage /> : <Navigate to={"/"} />} />
 		<Route path='/signup' element={!user ? <SignupPage /> : <Navigate to={"/"} />} />
 		<Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
+		<Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+		<Route path='/history' element={user ? <HistoryPage /> : <Navigate to={"/login"} />} />
+		<Route path='/*' element={<NotFoundPage />} />
+
+
 
       </Routes>
       <Footer/>
